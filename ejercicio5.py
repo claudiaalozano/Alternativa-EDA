d701e07396aa9093351da48d36e5a03b35224f1c
Carlaav <carla2002.av@gmail.com>
2022-02-17 13:36:38 +0100

    a

---
+++ ejercicio5.py
@@ -0,0 +1,13 @@
+niños=5
+precio= 20*niños
+if niños<2:
+    print(precio)
+if niños==2:
+    print(precio-precio*0.1)
+if niños==3:
+    print(precio-precio*0.15)
+if niños==4:
+    print(precio-precio*0.18)  
+if niños>=5:
+    print(precio)
+    
